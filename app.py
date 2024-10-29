4615ff4726c2242e036cf15cdb2be348661d9455
sebastian123gonzalo <sebastian123gonzalo@gmail.com>
2024-10-28 21:22:26 -0500

    second commit

--- app.py
+++ app.py
@@ -3,8 +3,16 @@ import pandas as pd
 from scripts.kruskal import kruskal
 from scripts.prim import prim
 from scripts.create_graph import create_graph
+from scripts.compare_algorithms import compare_algorithms
+import time
 
 app = Flask(__name__)
+
+def measure_time(algorithm, df):
+    start_time = time.time()
+    edges, algorithm_time, path = algorithm(df)
+    drawing_time = time.time() - start_time
+    return edges, algorithm_time, drawing_time, path
 
 @app.route('/')
 def index():
@@ -40,6 +48,12 @@ def optimize():
                                image_load_time=drawing,
                                algorithm_load_time=algorithm,
                                image_path=path)
+    elif algorithm == 'compare':
+        comparison_results = compare_algorithms(df)
+        return render_template('compare_algorithms.html',
+                               kruskal_details=comparison_results['kruskal_details'],
+                               prim_details=comparison_results['prim_details'])
+         
     else:
         return render_template('index.html')
 
---
+++ scripts/compare_algorithms.py
@@ -0,0 +1,25 @@
+from scripts.kruskal import kruskal
+from scripts.prim import prim
+
+def compare_algorithms(df):
+    # Ejecutar el algoritmo de Kruskal y medir el tiempo
+    edges_kruskal, algorithm_time_kruskal, drawing_time_kruskal, _ = kruskal(df)
+    
+    # Ejecutar el algoritmo de Prim y medir el tiempo
+    edges_prim, algorithm_time_prim, drawing_time_prim, _ = prim(df)
+
+    # Devolver los detalles de ambos algoritmos
+    return {
+        'kruskal_details': {
+            'algorithm_name': 'Kruskal',
+            'edges_count': edges_kruskal,
+            'image_load_time': drawing_time_kruskal,
+            'algorithm_load_time': algorithm_time_kruskal
+        },
+        'prim_details': {
+            'algorithm_name': 'Prim',
+            'edges_count': edges_prim,
+            'image_load_time': drawing_time_prim,
+            'algorithm_load_time': algorithm_time_prim
+        }
+    }
